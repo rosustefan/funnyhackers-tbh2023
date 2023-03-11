491bf953ce92243dd0553c62b0660428c3b75ac1
Ștefan Roșu <44987333+rosustefan@users.noreply.github.com>
2023-03-11 13:21:18 +0000

    Update requirements.txt & add docstring to Frontend

--- Frontend/Frontend.py
+++ Frontend/Frontend.py
@@ -1,6 +1,11 @@
 import gradio as gr
 
+
 def input_whitepaper(whitepaper_url):
+    """
+    This function receives the url for a blockchain's whitepaper,
+    sends it to the backend which returns its summary.
+    """
     # print(whitepaper_url)
     # call summarize function and return summary
     summary = ""
